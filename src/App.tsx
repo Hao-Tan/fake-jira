52652a3843cc8e7906e5b227766edfd2effbc5e1
Frank Tan <909628250@qq.com>
2022-03-11 02:18:27 +0800

    ci: 初始化项目

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from 'logo.svg'
+import { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useDebounce = (value: unknown, delay: number) => {
+    const [debouncedValue, setdebouncedValue] = useState(value)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setdebouncedValue(value)
+        }, delay)
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [value, delay])
+
+    return debouncedValue
 }
 
-export default App;
+function App() {
+    const [count, setCount] = useState(0)
+    const val = useDebounce(count, 500)
+    console.log(val, count)
+
+    useEffect(() => {
+        console.log(val)
+        return () => {
+            console.log(val)
+        }
+    }, [val])
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p
+                    onClick={() => {
+                        setCount(count + 1)
+                    }}
+                >
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    )
+}
+
+export default App
